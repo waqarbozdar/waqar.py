f3eeccbce9ddd6f1c879a24b2b0f04c2e49bcde3
waqarbozdar <85639592+waqarbozdar@users.noreply.github.com>
2021-06-11 01:36:45 +0500

    Waqar.py
    https://github.com/waqarbozdar/waqar.git
    Cd waqar
    is
    Phyton2.waqar.py

---
+++ Waqar.py
@@ -0,0 +1,43 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+readme = open(os.path.join(here, 'README.rst')).read()
+
+requires = [
+    'pytest',
+]
+
+
+def get_version():
+    p = os.path.join(os.path.dirname(os.path.abspath(__file__)), "VERSION.txt")
+    with open(p) as f:
+        return f.read().strip()
+
+
+setup(
+    name='CHAMPION KILLER GANG',
+    version=get_version(),
+    description='Pytest testing utilities with docker containers.',
+    long_description=readme,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 2.7",
+    ],
+    author='Lovely Systems',
+    author_email='office@lovelysystems.com',
+    url='https://github.com/lovelysystems/lovely-pytest-docker',
+    keywords='pytest testing docker compose',
+    namespace_packages=['lovely'],
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    zip_safe=False,
+    include_package_data=True,
+    install_requires=requires,
+    entry_points={
+        'CHAMPION KILLER GANG': [
+            'lovely_pytest_docker = lovely.pytest.docker.compose',
+        ],
+    },
+)
